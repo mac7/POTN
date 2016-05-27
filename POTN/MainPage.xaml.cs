83264c6aa2958bc030ec8a458aaeb8a3ad3ca9c9
Maxim Sosedov <Maxim Sosedov>
2016-05-27 14:58:26 +0300

    start

---
+++ POTN/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace POTN
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            List<string> ranges = new List<string>() { "5Ghz", "2GHz" };
+            List<string> bands = new List<string>() { "10", "20", "40" };
+            
+            foreach (var range in ranges)
+            {
+                comboBox_range.Items.Add(range);
+            }
+   
+            foreach (var band in bands)
+            {
+                comboBox_band.Items.Add(band);
+            }
+
+
+        }
+    }
+}
