4278e9192d5cbc91c86163402d4f41b4ee8f3bea
mac7 <sosedow@hotmail.com>
2016-05-30 11:59:52 +0300

    bands calc *with BUG*

--- POTN/MainPage.xaml.cs
+++ POTN/MainPage.xaml.cs
@@ -26,7 +26,7 @@ namespace POTN
         public MainPage()
         {
             this.InitializeComponent();
-            List<string> ranges = new List<string>() { "5Ghz", "2GHz" };
+            List<string> ranges = new List<string>() { "5Ghz" };
             List<string> bands = new List<string>() { "10MHz", "20MHz", "40MHz" };
             
             foreach (var range in ranges)
@@ -39,30 +39,64 @@ namespace POTN
                 comboBox_band.Items.Add(band);
             }
 
-            outputBox.Text += "\n\n\n Now works only for 5GHz range";
+            outputBox.Text += "\n\n Now works only for 5GHz range";
 
         }
 
         private List<double> PowerCalculation(string range, string band, Int32 gain)
         {
-            double powerInWatt = Math.Pow(10, (-gain-3)/10.0);
-            double powerInDBm = 10 * Math.Log10(powerInWatt*1000);
-            return new List<double>() { Math.Round(powerInWatt, 4), powerInDBm };
+            switch (band)
+            {
+                case "10MHz":
+                    return Powers(gain -= 3); //bug here
+                   
+                case "20MHz":
+                    return Powers(gain); //bug here
+
+                case "40MHz":
+                    return Powers(gain += 3); //bug here
+
+                default:
+                    return new List<double>() { 0.00001, 0.00001, 0.00001 }; ;
+  
+            }
+
+        }
+
+        private List<double> Powers (Int32 gain)
+        {
+            double powerInWatt = Math.Pow(10, (-gain) / 10.0);
+            double powerInDBm = 10 * Math.Log10(powerInWatt * 1000);
+            double powerInDBW = powerInDBm - 30;
+            return new List<double>() { Math.Round(powerInWatt, 4), powerInDBm, powerInDBW };
         }
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
-            var result = PowerCalculation ( comboBox_range.SelectedItem.ToString(), comboBox_band.SelectedItem.ToString(), Convert.ToInt32(textBox_gain.Text));
-            StringBuilder output = new StringBuilder();
-            output.Append("Maximum transmitter power, Watt: ");
-            output.Append(result[0].ToString() + "\n");
-            output.Append("Maximum transmitter power, dBm: ");
-            output.Append(result[1].ToString());
+            try
+            {
+                var result = PowerCalculation(comboBox_range.SelectedItem.ToString(), comboBox_band.SelectedItem.ToString(), Convert.ToInt32(textBox_gain.Text));
+                StringBuilder output = new StringBuilder();
+                output.Append("Maximum transmitter power, Watt: ");
+                output.Append(result[0].ToString() + "\n");
+                output.Append("Maximum transmitter power, dBm: ");
+                output.Append(result[1].ToString() + "\n");
+                output.Append("Maximum transmitter power, dBW: ");
+                output.Append(result[2].ToString());
+                outputBox.Text = output.ToString();
+            }
+            catch (System.NullReferenceException)
+            {
+
+                outputBox.Text = "--- pick all values ---";
+            }
+            
+            
             
 
 
 
-            outputBox.Text = output.ToString();
+            
         
         }
     }
